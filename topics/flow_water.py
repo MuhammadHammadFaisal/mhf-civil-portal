1a6f2439ee5b294c5a1223cb79b6f0b4cdf6d9b5
MuhammadHammadFaisal <hammadbinfaisal@gmail.com>
2026-01-24 14:47:16 +0500

    Update flow_water.py

--- topics/flow_water.py
+++ topics/flow_water.py
@@ -121,7 +121,7 @@ def app():
            
             # Top of Soil (Split for tube)
             ax.plot([soil_x, neck_x], [datum_y + val_z , datum_y + val_z], color=wall_color, lw=wall_thick, zorder=2)
-            ax.plot([tube_start_x + neck_w, soil_x + soil_w], [datum_y + val_z , datum_y + val_z], color=wall_color, lw=wall_thick, zorder=2) 
+            ax.plot([tube_start_x + neck_x, soil_x + soil_w], [datum_y + val_z , datum_y + val_z], color=wall_color, lw=wall_thick, zorder=2) 
             # Bottom Tube & Left Tank Walls (Continuous Path)
             
             # Inner Path (Left side of tube -> Bottom -> Left Tank Left -> Top)
