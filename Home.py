4c6bf5082c497f34dbdaf33d913f8050e8806042
MuhammadHammadFaisal <hammadbinfaisal@gmail.com>
2026-01-24 17:18:14 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,15 +3,15 @@ import streamlit as st
 # 1. PAGE CONFIG (Must be the first command)
 st.set_page_config(
     page_title="MHF Civil Portal", 
-    page_icon="assets/Logo.png",  # <--- [CHANGE 1] Replaced emoji with your Logo path
+    page_icon="assets/logo.png",  # <--- [CHANGE 1] Replaced emoji with your logo path
     layout="wide"
 )
 
 # 2. THE BRANDING & BIO
 def main():
-    # --- [CHANGE 2] ADD SIDEBAR Logo HERE ---
-    # This puts the Logo nicely at the top left of the sidebar
-    st.Logo("assets/Logo.png", icon_image="assets/Logo.png")
+    # --- [CHANGE 2] ADD SIDEBAR logo HERE ---
+    # This puts the logo nicely at the top left of the sidebar
+    st.logo("assets/logo.png", icon_image="assets/logo.png")
     # ----------------------------------------
 
     st.markdown("# MHF Civil Portal")
@@ -58,4 +58,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
