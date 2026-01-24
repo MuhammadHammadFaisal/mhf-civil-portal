f8da6b7c2b25cd53710eb594b494691ee61d07fc
MuhammadHammadFaisal <hammadbinfaisal@gmail.com>
2026-01-24 17:39:13 +0500

    Update 04_Fluid_Mechanics.py

--- pages/04_Fluid_Mechanics.py
+++ pages/04_Fluid_Mechanics.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+def app():
+    st.set_page_config(page_title="Module Under Construction", page_icon="🚧")
+    
+    st.title("🚧 Module Under Construction")
+    st.markdown("---")
+
+    col1, col2 = st.columns([1, 2])
+    
+        
+    st.info("Expected Release: Fall 2026")
+    st.progress(0.1, text="Coding in progress...")
+
+if __name__ == "__main__":
+    app()
